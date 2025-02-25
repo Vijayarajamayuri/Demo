280f9ff24f5d932c257cee089e45fba440f50495
mayuri vijayaraja akula <46686521+Vijayarajamayuri@users.noreply.github.com>
2025-02-25 11:34:08 -0800

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "azureusername" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.admin_username
-}
-
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
-}
